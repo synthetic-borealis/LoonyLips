628f8db5c2b6062deca9a333a4df910570a6dcc4
Elhanan Flesch <snip.coloured@gmail.com>
2019-10-24 20:49:35 +0300

    6 Appending arrays and if conditions

--- LoonyLips.gd
+++ LoonyLips.gd
@@ -1,19 +1,45 @@
 extends Control
 
+var player_words = []
+var prompts = ["a name", "a noun", "an adverb", "an adjective"]
+var story = "Once upon a time %s ate %s flavoured sandwich which made him feel all %s. It was a %s day."
+
+onready var PlayerText = $VBoxContainer/HBoxContainer/PlayerText
+onready var DisplayText = $VBoxContainer/DisplayText
+
 func _ready():
-		var prompts = ["I", "pizza", "pineapple", "delicious"]
-		var story = "Once upon a time %s ate %s with %s and it was %s"
-		
-		$VBoxContainer/DisplayText.text = story % prompts
+	DisplayText.text = "Welcome to Loony Lips! We're going to tell a story and have wonderous time.\n"
+	check_player_words_length()
+
 
 func _on_PlayerText_text_entered(new_text):
-	update_DisplayText(new_text)
-	
-func update_DisplayText(words):
-	$VBoxContainer/DisplayText.text = words
-	$VBoxContainer/HBoxContainer/PlayerText.clear()
+	add_to_player_words()
 
 
 func _on_TextureButton_pressed():
-	var words = $VBoxContainer/HBoxContainer/PlayerText.text
-	update_DisplayText(words)
+	add_to_player_words()
+
+
+func add_to_player_words():
+	player_words.append(PlayerText.text)
+	DisplayText.text = ""
+	PlayerText.clear()
+	check_player_words_length()
+
+
+func is_story_done():
+	return player_words.size() == prompts.size()
+
+
+func check_player_words_length():
+	if is_story_done():
+		tell_story()
+	else:
+		prompt_player()
+
+
+func tell_story():
+	DisplayText.text = story % player_words
+
+func prompt_player():
+	DisplayText.text += "May I have " + prompts[player_words.size()] + " please?"
